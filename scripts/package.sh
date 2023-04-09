06e7e5d3138acc4babe0c6ac992bf64cdb138e51
everlaster <everlaster@protonmail.com>
2023-04-09 23:16:10 +0300

    update packaging script

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-author_name=everlaster
+author_name=VamTimbo
 resource_name=Extraltodeus-ExpressionRND
 
 package_version=$1
@@ -15,23 +15,10 @@ plugin_version=$(git describe --tags --match "v*" --abbrev=0 HEAD 2>/dev/null | 
 work_dir=publish
 mkdir -p work_dir
 
-resource_dir=$work_dir/Custom/Scripts/$author_name/$resource_name
-mkdir -p $resource_dir
-cp meta.json $work_dir/
-cp ./*.cslist $resource_dir/
-cp -r src $resource_dir/
-cp -r Vendor $resource_dir/
-
-# update version info
-sed -i "s/0\.0\.0/$plugin_version/g" $work_dir/meta.json
-sed -i "s/0\.0\.0/$plugin_version/g" $resource_dir/src/$resource_name.cs
-
-for file in $(find $resource_dir -type f -name "*.cs"); do
-    # set production env
-    sed -i "s/#define ENV_DEVELOPMENT/\/\//" "$file"
-    # hide .cs files (plugin is loaded with .cslist)
-    touch "$file".hide
-done
+resource_dir="$work_dir/Custom/Scripts/__Frequently Used"
+mkdir -p "$resource_dir"
+cp meta.json "$work_dir/"
+cp ExpressionRandomizer_1.7_Timbo.cs "$resource_dir/"
 
 # zip files to .var and cleanup
 printf "Creating package...\n"
