94ffcee01510e8e098fb4e50bb0ff57b1a2a42e8
everlaster <everlaster@protonmail.com>
2023-04-11 02:24:42 +0300

    go back to packaging inside single __Frequently Used dir (multiple dirs causes VAM file browser to stop navigating automatically further than Custom/Scripts)

--- scripts/package.sh
+++ scripts/package.sh
@@ -13,20 +13,17 @@ plugin_version=$(git describe --tags --match "v*" --abbrev=0 HEAD 2>/dev/null | 
 # packaging
 work_dir=publish
 mkdir -p "$work_dir"
-cp meta.json "$work_dir/"
+cp meta.json "$work_dir"/
 
-legacy_resource_dir="$work_dir/Custom/Scripts/__Frequently Used"
-mkdir -p "$legacy_resource_dir"
-cp "$expression_randomizer"_1.7_Timbo.cs "$legacy_resource_dir/"
-
-resource_dir="$work_dir/Custom/Scripts/everlaster/Extraltodeus-ExpressionRND"
+resource_dir="$work_dir/Custom/Scripts/tmp"
 mkdir -p "$resource_dir"
-cp "$expression_randomizer.cslist" "$resource_dir/"
-cp -r src "$resource_dir/"
+cp "$expression_randomizer"_1.7_Timbo.cs "$resource_dir"/
+cp "$expression_randomizer".cslist "$resource_dir"/
+cp -r src "$resource_dir"/
 
 # update version info
-sed -i "s/0\.0\.0/$plugin_version/g" "$work_dir/meta.json"
-sed -i "s/0\.0\.0/$plugin_version/g" "$resource_dir/src/$expression_randomizer.cs"
+sed -i "s/0\.0\.0/$plugin_version/g" "$work_dir"/meta.json
+sed -i "s/0\.0\.0/$plugin_version/g" "$resource_dir"/src/$expression_randomizer.cs
 
 for file in $(find "$resource_dir" -type f -name "*.cs"); do
     # set production env
@@ -34,6 +31,8 @@ for file in $(find "$resource_dir" -type f -name "*.cs"); do
     # hide .cs files (plugin is loaded with .cslist)
     touch "$file".hide
 done
+
+mv "$work_dir/Custom/Scripts/tmp" "$work_dir/Custom/Scripts/__Frequently Used"
 
 # zip files to .var and cleanup
 printf "Creating package...\n"
